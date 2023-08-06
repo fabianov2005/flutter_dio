e8f3b6c40ee0b44d49fe4726964a2b5f00197911
Fabiano Augusto <fabiano-augusto.silva@serpro.gov.br>
2023-08-05 21:15:06 -0300

    Projeto Dart Console.

---
+++ dart_dio/meu_app_console_dart/bin/meu_app_console_dart.dart
@@ -0,0 +1,5 @@
+import 'package:meu_app_console_dart/meu_app_console_dart.dart' as meu_app_console_dart;
+
+void main(List<String> arguments) {
+  print('Hello world: ${meu_app_console_dart.calculate()}!');
+}
